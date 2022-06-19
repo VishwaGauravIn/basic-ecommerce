3a44b43b7cd23a179a703954104bc688ce39f56b
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-06-19 21:59:30 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-import Header from './containers/Header';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
+import { useSelector } from "react-redux";
 
 function App() {
+  const products = useSelector((state) => state)
+  console.log(products)
   return (
     <div className="App">
-      <Header/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing/>} />
+          <Route path="/product/:ProductId" element={<ProductDetails/>} />
+          <Route>404 Not found!</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
